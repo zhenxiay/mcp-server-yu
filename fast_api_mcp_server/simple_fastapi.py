bc891fe3e121a53f7064586088a2e5fc7eff4a87
Yu Zhenxiao (MA/BDO72) <Zhenxiao.Yu@de.bosch.com>
2025-07-23 17:29:30 +0200

    update port setting in FastAPI application to 8080

--- fast_api_mcp_server/simple_fastapi.py
+++ fast_api_mcp_server/simple_fastapi.py
@@ -8,4 +8,4 @@ def read_hello():
     return {"message": "Hello, world!"}
 
 if __name__ == "__main__":
-    uvicorn.run("simple_fastapi:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("simple_fastapi:app", host="0.0.0.0", port=8080, reload=True)
